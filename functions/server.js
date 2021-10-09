09272d45d25f6d20045f6f11c0217a8c39d86fce
mehruddin khan <77490569+mehruddin11@users.noreply.github.com>
2021-10-09 13:30:12 +0530

    Add files via upload

--- functions/server.js
+++ functions/server.js
@@ -6,45 +6,36 @@ const app = expess();
 const router =expess.Router();
 app.use(cors());
 app.use(bodyParser.json());
-const database = {
-	users:[
-	{
-
-	id:1900,
-	name:'beautiful_nature',
-	date:new Date,
-	img:'https://th.bing.com/th/id/OIP.X_65uIJkSF8bJl_zyU4twgHaEo?w=269&h=180&c=7&r=0&o=5&pid=1.7'
-
-	}],
-
-	producst:[
-	{
-
-	id:19001,
-	name:'sunset',
-	date:new Date,
-	img:'https://th.bing.com/th/id/OIP.OcdKodm_a3FiAYKlUAjFdgHaE8?w=252&h=180&c=7&r=0&o=5&pid=1.7'
-
-	}],
-	name:[{
-
-	id:19002,
-	name:'clean-road',
-	date:new Date,
-	img:'https://th.bing.com/th/id/OIP.OcdKodm_a3FiAYKlUAjFdgHaE8?w=252&h=180&c=7&r=0&o=5&pid=1.7'
-
-	}
-	],
-	age:[
-	{
-
-	id:19003,
-	name:'beautiful-birdge',
-	date:new Date,
-	img:'https://th.bing.com/th/id/OIP._h7s27M_cYLoJ7SzE7XRZQHaEK?w=300&h=180&c=7&r=0&o=5&pid=1.7'
-
-	}]
+const database = [
+{
+"id": "rec1JZlfCIBOPdcT2",
+"title": "Samsung Galaxy S8",
+"price": "399.99",
+"img": "https://dl.airtable.com/.attachments/64b266ad865098befbda3c3577a773c9/24497852/yedjpkwxljtb75t3tezl.png",
+"amount": 1
+},
+{
+"id": "recB6qcHPxb62YJ75",
+"title": "google pixel",
+"price": "499.99",
+"img": "https://dl.airtable.com/.attachments/91c88ae8c1580e2b762ecb3f73ed1eed/a633139a/phone-1_gvesln.png",
+"amount": 1
+},
+{
+"id": "recdRxBsE14Rr2VuJ",
+"title": "Xiaomi Redmi Note 2",
+"price": "699.99",
+"img": "https://dl.airtable.com/.attachments/bae9208dc34f35128749ecda5b999e84/337c285d/phone-3_h2s6fo.png",
+"amount": 1
+},
+{
+"id": "recwTo160XST3PIoW",
+"title": "Samsung Galaxy S7",
+"price": "599.99 ",
+"img": "https://dl.airtable.com/.attachments/91ee456448cef47deec553a2ea3fa8ad/b08bec68/phone-2_ohtt5s.png",
+"amount": 1
 }
+]
 router.get('/', (req, res)=>{
 	res.json(database)
 });
